5552fae54f8004cb674b2d3a243355a741d8354b
jianghailongdroi1 <853226549@qq.com>
2020-02-12 10:02:17 +0800

    修复一个bug

--- autotest/myUtil/commonFunction.py
+++ autotest/myUtil/commonFunction.py
@@ -1,4 +1,4 @@
-import datetime
+import datetime,json
 #遍历字典中的所有value，如果遇到datetime类型的，转化为字符串
 def turn_dic_to_be_JSON_serializable(dic):
     # print("dic:", dic)
@@ -10,3 +10,15 @@ def turn_dic_to_be_JSON_serializable(dic):
             dic[key] = dic[key].strftime("%Y-%m-%d %H:%M:%S")
 
     return dic
+
+class MyEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+        """
+        只要检查到了是bytes类型的数据就把它转为str类型
+        :param obj:
+        :return:
+        """
+        if isinstance(obj, bytes):
+            return str(obj, encoding='utf-8')
+        return json.JSONEncoder.default(self, obj)
